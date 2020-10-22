e9d01c9a2871e34e8fe7760b83c117fce5dd83a9
Luna-matana <luis.fortes01@gmail.com>
2020-10-22 15:52:15 -0300

    Retirand
    Retirar todos os arquivos e reiniciar o projeto já com orientação a objetos

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-<!-- sidebar -->  
-<img src="images/marca.png" />
-<nav class="nav flex-column" >
-    <a href="adm.php" class="nav-link" >Filmes</a>
-    <a href="cliente.php" class="nav-link">Clientes</a>        
-    <a href="funcionario.php" class="nav-link">Funcionarios</a>
-    <a href="sair.php" class="nav-link">Sair</a>
-</nav>  
-<!-- /sidebar -->
