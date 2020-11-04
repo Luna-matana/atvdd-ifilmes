ffc5fff60de6da9a280366cbe064f1890b948083
Luna-matana <luis.fortes01@gmail.com>
2020-11-03 23:08:00 -0300

    Git Ignore

--- models/Conexao.php
+++ models/Conexao.php
@@ -7,7 +7,7 @@ class Conexao{
     function get_conexao(){
         $opcoes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
         try {
-        $this->con = new PDO("mysql:host=localhost;dbname=localdb", "root", "holy2905", $opcoes);
+        $this->con = new PDO("mysql:host=localhost;dbname=localdb", "root", "");
         } catch (PDOException $erro) {
                 echo $erro -> getMessage();
         }
